d2b2eca88ef07254837b0d9c509803c7241599e3
Gabriel Ramos <whosramoss@gmail.com>
2025-02-06 17:56:33 -0300

    chore: update app

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
-
+import { ThemeProvider } from "styled-components"
+import { defaultTheme } from "./styles/theme/default"
+import { GlobalStyle } from "./styles/global"
+import { Router } from "./Routes"
+import { BrowserRouter } from "react-router-dom"
+import { CartContextProvider } from "./contexts/CartContext"
 
 export function App() {
    return (
-      <div>hello</div>
+      <ThemeProvider theme={defaultTheme}>
+         <GlobalStyle />
+         <BrowserRouter>
+            <CartContextProvider>
+               <Router />
+            </CartContextProvider>
+         </BrowserRouter>
+      </ThemeProvider>
    )
 }
